c4f7ebbad0526157ed03aa3ebd6c51b2dc43c9eb
matheusfna <matheus_guga@hotmail.com>
2025-03-10 11:29:59 -0300

    Adicionar arquivos de projeto.

---
+++ ExerciciosProposto03/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExerciciosProposto03 {
+    class URI {
+        static void Main(string[] args) {
+
+            int n;
+
+            n = int.Parse(Console.ReadLine());
+
+            int[] a = new int[n];
+            int[] b = new int[n];
+
+            //Vetor A
+            string[] valor = Console.ReadLine().Split(' ');
+            for(int i = 0; i < n; i++) {
+                a[i] = int.Parse(valor[i]);
+            }
+
+            //Vetor B com
+            valor = Console.ReadLine().Split(' ');
+            for(int i = 0; i < n; i++) {
+                b[i] = int.Parse(valor[i]);
+            }
+
+            //Criando o Vetor C para fazer a soma do A + B
+            int[] c = new int[n];
+
+            for(int i = 0; i < n; i++) {
+                c[i] = a[i] + b[i];
+            }
+
+            //Mostrando a soma dos vetores A + B na tela
+            for(int i = 0; i < n; i++) {
+                Console.WriteLine(c[i] + " ");
+            }
+        }
+    }
+}
+
+
+
